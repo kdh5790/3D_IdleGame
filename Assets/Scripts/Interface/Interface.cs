71fa8345558da4b4fb53d863b8505a110be10c75
kdh5790 <k_0106@naver.com>
2025-03-25 22:15:49 +0900

    [ Feat ] 플레이어 데미지 처리 추가

--- Assets/Scripts/Interface/Interface.cs
+++ Assets/Scripts/Interface/Interface.cs
@@ -1,8 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
 public interface IConsumableItem
 {
     public void UsePotion(float percentage = 0, float duration = 0);
+}
+
+public interface IDamageable
+{
+    void OnDamaged(BigInteger damage);
 }
--- Assets/Scripts/Player/PlayerStatus.cs
+++ Assets/Scripts/Player/PlayerStatus.cs
@@ -4,14 +4,15 @@ using System.Collections.Generic;
 using System.Numerics;
 using UnityEngine;
 
-public class PlayerStatus : MonoBehaviour
+public class PlayerStatus : MonoBehaviour, IDamageable
 {
-    [SerializeField] private BigInteger maxHealth = 525430; // 최대 체력 
+    [SerializeField] private BigInteger maxHealth = 1500; // 최대 체력 
     private BigInteger currentHealth; // 현재 체력
 
-    [SerializeField] private BigInteger gold = 32500;
-    [SerializeField] private BigInteger attackPower = 3532510; // 공격력 
-    [SerializeField] private float speed; // 이동 속도
+    [SerializeField] private BigInteger gold = 0;
+    [SerializeField] private BigInteger attackPower = 200; // 공격력 
+
+    private bool isInvincibility = false;
 
     public Action OnStatusChanged;
 
@@ -63,6 +64,54 @@ public class PlayerStatus : MonoBehaviour
 
     private void Start()
     {
-        currentHealth = maxHealth;
+        CurrentHealth = MaxHealth;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            OnDamaged(50);
+        }
+    }
+
+    public void OnDamaged(BigInteger damage)
+    {
+        if (isInvincibility) return;
+
+        CurrentHealth = BigInteger.Max(0, CurrentHealth - damage);
+        StartCoroutine(ApplyInvincibility(0.3f));
+
+        if(CurrentHealth == 0)
+        {
+            // 캐릭터 사망
+        }
+    }
+
+    private IEnumerator ApplyInvincibility(float time)
+    {
+        isInvincibility = true;
+        MeshColorChange(Color.red);
+
+        yield return new WaitForSeconds(time);
+
+        isInvincibility = false;
+        MeshColorChange(Color.white);
+    }
+
+    private void MeshColorChange(Color color)
+    {
+        MeshRenderer[] meshRenderers = GetComponentsInChildren<MeshRenderer>(true);
+        SkinnedMeshRenderer[] skinnedMeshRenderers = GetComponentsInChildren<SkinnedMeshRenderer>(true);
+
+        foreach(MeshRenderer renderer in meshRenderers)
+        {
+            renderer.material.color = color;
+        }
+
+        foreach (SkinnedMeshRenderer renderer in skinnedMeshRenderers)
+        {
+            renderer.material.color = color;
+        }
     }
 }
