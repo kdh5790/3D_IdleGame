6d0b7c946dfca6bfeffaa3ec677bc124bb7742b1
kdh5790 <k_0106@naver.com>
2025-03-26 02:20:31 +0900

    [ Feat ] EnemyAI 추가

--- Assets/Scripts/Enemy/EnemyAI.cs
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -1,18 +1,87 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private NavMeshAgent agent;
+    private Player player;
+    private EnemyAnimationData animationData;
+    private Animator animator;
+
+    [SerializeField] private float attackRange = 2f;
+    [SerializeField] private float attackCooldown = 1f;
+    private float attackTimer = 0f;
+
+
+    private void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        player = Player.Instance;
+
+        animationData.Initialize();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        attackTimer -= Time.deltaTime;
+
+        float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+        if (distanceToPlayer < 5f)
+        {
+            if (distanceToPlayer <= attackRange)
+            {
+                AttackTarget();
+            }
+            else
+            {
+                MoveToTarget();
+            }
+        }
+        else
+            TransitionToIdle();
+    }
+
+    private void MoveToTarget()
+    {
+        agent.SetDestination(player.transform.position);
+        StartAnimation(animationData.RunParameterHash);
+        StopAnimation(animationData.IdleParameterHash);
+    }
+
+    private void AttackTarget()
+    {
+        agent.ResetPath();
+        StopAnimation(animationData.RunParameterHash);
+
+        if (attackTimer <= 0f)
+        {
+            attackTimer = attackCooldown;
+
+            StartAnimation(animationData.AttackParameterHash);
+        }
+    }
+
+    private void TransitionToIdle()
+    {
+        agent.ResetPath();
+        StopAnimation(animationData.RunParameterHash);
+        StartAnimation(animationData.IdleParameterHash);
+    }
+
+    public void ResetAttack()
+    {
+        StopAnimation(animationData.AttackParameterHash);
+    }
+
+    public void StartAnimation(int animatorHash)
+    {
+        animator.SetBool(animatorHash, true);
+    }
+
+    public void StopAnimation(int animatorHash)
+    {
+        animator.SetBool(animatorHash, false);
     }
 }
